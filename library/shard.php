81026e36b633a3acc9760343f887c3ae22e69e87
Terry Geeksen <geeksen@gmail.com>
2014-04-14 02:09:35 +0000

    shard

--- library/shard.php
+++ library/shard.php
@@ -2,14 +2,11 @@
 
 class shard
 {
-	var $config = array
-	(
-		'count' => 1,
-		'format' => '%d',
-	);
+	var $shard_count = 1;
+	var $postfix_format = '%d';
 
 	function postfix($shard_key)
 	{
-		return sprintf($this->config['format'], ($shard_key % $this->config['count']));
+		return sprintf($this->postfix_format, ($shard_key % $this->shard_count));
 	}
 }
